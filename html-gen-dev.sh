0b453e97ccc1b38ca12d005d889cb5ee53246e1e
Exybore <exybore@becauseofprog.fr>
2017-11-01 20:26:49 +0100

    Cleaner code

--- html-gen-dev.sh
+++ html-gen-dev.sh
@@ -1,7 +1,7 @@
-echo "Langue - Language :
-1) Français
-2) English"
-echo -n "Votre choix | Your choice: "
+echo "Language | Langue:
+1) English (default)
+2) Français"
+echo -n "Your choice | Votre choix: "
 read lang
 if [ $lang = 1 ] && [ -n $lang ]; then
   lang=("Bienvenue dans le générateur de fichier HTML par Exybore !" "Nom du fichier (par défaut: index.html): " "Auteur: " "URL: " "Titre: " "Description: " "Votre fichier a bien été généré dans $PWD !")
@@ -14,56 +14,12 @@ echo ${lang[0]}
 echo -n ${lang[1]}
 read file
 
-if [ -z $file ]
-then
+if [ -z $file ]; then
   file="index.html"
 fi
 
-# echo -n ${lang[2]}
-# read author
-
-# if [ -n $author ]
-# then
-#   author_meta="<meta name=\"author\" content=\"$author\" />"
-# else
-#   author_meta=""
-# fi
-
-# echo -n ${lang[3]}
-# read url
-
-# if [ -n $url ]
-# then
-#   url_meta="<meta property=\"og:url\" content=\"$url\""
-#   url_title="<a href=\"$url\">$url</a>"
-# else
-#   url_meta=""
-#   url_title=""
-# fi
-
 echo -n ${lang[4]}
 read title
-
-if [ -n $title ]
-then
-  title_meta="<meta property=\"og:title\" content=\"$title\" />"
-#   title_title="<h1>$title</h1>"
-else
-  title_meta=""
-#   title_title=""
-fi
-
-# echo -n ${lang[5]}
-# read description
-
-# if [ -n $description ]
-# then
-#   description_meta="<meta property=\"og:description\" content=\"$description\""
-#   description_title="<p>$description</p>"
-# else
-#   description_meta=""
-#   description_title=""
-# fi
 
 echo '' >> $file
 echo "<!--
@@ -75,12 +31,33 @@ echo "<!--
 <html lang=\"$locale\">
 <head>
   <title>$title</title>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  $title_meta
-</head>
+  <meta charset=\"UTF-8\"/>
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
+  <meta property=\"og:title\" content=\"$title\"/>" >> $file
+echo -n ${lang[2]}
+read author
+
+if [ -n $author ]; then
+  echo "  <meta name=\"author\" content=\"$author\"/>" >> $file
+fi
+
+echo -n ${lang[3]}
+read url
+
+if [ -n $url ]; then
+  echo "  <meta property=\"og:url\" content=\"$url\"/>" >> $file
+fi
+echo -n ${lang[5]}
+read description
+
+if [ -n $description ]; then
+  echo "  <meta property=\"og:description\" content=\"$description\"/>" >> $file
+fi
+
+echo "</head>
 <body>
 
 </body>
 </html>" >> $file
+
 echo ${lang[6]}
